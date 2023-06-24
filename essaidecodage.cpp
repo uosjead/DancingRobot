f66cf4d565bb92c99c743d83c0a09325f89ccda6
uosjeaed <wrpearson9@gmail.com>
2023-06-24 14:32:36 +0000

    added initial file

---
+++ essaidecodage.cpp
@@ -0,0 +1,38 @@
+
+// can't remember this question...
+
+int s = abs(numbers[0] - numbers[1]);
+for(int i=0; i < numbers.zie() -1; i++) {
+    for (int j = i +1; i < numbers.size(); j++) {
+        if (abs(numbers[i] - numbers[j])) < s) {
+            s = abs(numbers[i] - numbers[j]);
+        }
+     }
+}
+
+
+// something to do with unsigning?
+return (value >> pose) & 1;
+
+
+// dancing robot maybe?
+
+int p = 0;
+
+stack<int> stack;
+int b2 = -2;
+
+for (int i = 1; i < 222222222; i++) {
+    if (i > 2) {
+        stack.push(p);
+        int b2_ = p + (b2 - b1);
+        p = p + (b2 - b1);
+        b2 = b2_;
+    }
+    else if (i == 1) {
+        p++;
+    }
+    else if (i == 2) {
+        p = p - 2;
+    }
+}
