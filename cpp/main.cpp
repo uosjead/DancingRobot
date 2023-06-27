e1f5f9ab02872f25a370c9e11acaeeaefedb593e
uosjead <wrpearson9@gmail.com>
2023-06-27 16:31:19 +0100

    changes to cpp code

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
-
+#include <stdlib.h>
 #include "dancingrobot.hpp"
 
-int main() {
+int main(int argc, char* argv[]) {
 	std::cout << "let's dance!\n";
-	int position = FindDancePosition(3);
+	char* input;
+	if (argc < 2) {
+		std::cout << "missing input argument postion\n";
+	//	input = "0".c_str();
+	        const char* d = "0";
+	        strcpy(input, d);
+	}
+	else {
+		input = argv[1];
+	}
+	int step = strtol(input, NULL, 10);
+	int position = FindDancePosition(step);
 	std::cout << "Dance position:" << position <<"\n";
 	return 0;
 }
