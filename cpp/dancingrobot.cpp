179b3cbed80b3693caa23a8bc925d00624bc3ca3
uosjeaed <wrpearson9@gmail.com>
2023-07-01 10:30:26 +0000

    adding algorithm code v1

--- cpp/dancingrobot.cpp
+++ cpp/dancingrobot.cpp
@@ -1,6 +1,36 @@
+#include <cstdlib>
+#include <vector>
+// #include <iostream>
 #include "dancingrobot.hpp"
 
 int FindDancePosition(int position) {
-	return position + 1;
+
+	int i = 0;
+	int current_position = 0;
+	std::vector<int> previous_changes;
+
+	while (i < abs(position)) {
+		if (i > 1) {
+			// std::cout << "at position " << i << " subtracting " << previous_changes[1] << " - " <<  previous_changes[0] << std::endl;
+			int delta = previous_changes[1] - previous_changes[0];
+			current_position += delta;
+			int last_change = previous_changes[1];
+			previous_changes.pop_back();
+			previous_changes.pop_back();
+			previous_changes.push_back(last_change);
+			previous_changes.push_back(delta);
+		}
+		else if (i == 0) {
+			current_position++;
+			previous_changes.push_back(1);
+		}
+		else if (i == 1) {
+			current_position -= 2;
+			previous_changes.push_back(-2);
+		}
+		i++;
+	}
+
+	return current_position;
 }
 
--- cpp/dancingrobot_test.cpp
+++ cpp/dancingrobot_test.cpp
@@ -2,12 +2,25 @@
 #include <stdio.h>
 #include "dancingrobot.hpp"
 
-#define ASSERT(x) { if (!(x)) {std::cout << __FUNCTION__ << " failed on line " << __LINE__ << std::endl; exit(1);} }
+#define ASSERT_EQUAL(x, y) { \
+    if (!(x == y)) {\
+        std::cout << __FUNCTION__ \
+            << " failed on line " << __LINE__ \
+            << " got: " << x << " expected: " << y \
+            << std::endl; \
+        exit(1);\
+    } \
+}
 
 void test_dancingrobot()
 {
-    ASSERT((FindDancePosition(0) == 1));
-    ASSERT((FindDancePosition(3) == 4));
+    ASSERT_EQUAL(FindDancePosition(0),  0);
+    ASSERT_EQUAL(FindDancePosition(1), 1);
+    ASSERT_EQUAL(FindDancePosition(2), -1);
+    ASSERT_EQUAL(FindDancePosition(3), -4);
+    ASSERT_EQUAL(FindDancePosition(4), -5);
+    ASSERT_EQUAL(FindDancePosition(5), -3);
+    ASSERT_EQUAL(FindDancePosition(6), 0);
 }
 
 int main(void) {
