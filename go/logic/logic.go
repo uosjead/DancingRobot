f3ed988b7df79d1ab9a04b271780f08c57ca02f7
uosjeaed <wrpearson9@gmail.com>
2023-07-11 16:08:22 +0100

    fixing pointer print

--- go/logic/logic.go
+++ go/logic/logic.go
@@ -3,5 +3,5 @@ package dancingrobot
 import "fmt"
 
 func Do(position *int) {
-	fmt.Printf("Hello, logic module. Position at %d\n", &position)
+	fmt.Printf("Hello, logic module. Position at %d\n", *position)
 }
